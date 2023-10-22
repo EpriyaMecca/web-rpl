2ffae385803404031ca4069e982549d2ebf950ca
EpriyaMecca <meccaepriya@gmail.com>
2023-10-22 20:17:48 +0700

    first commit

---
+++ news2.php
@@ -0,0 +1,81 @@
+<html>
+    <head>
+        <title>
+            WEBSITE PERDANA SAYA
+        </title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    </head>
+
+    <!-- Navbar -->
+    <body class="">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow p-2">
+  <div class="container">
+    <a class="navbar-brand" href="index.jpg">
+    <img src="img/logo.jpg" alt="logo" width="70" height="70" class="rounded-circle img-thumbnail"> 
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="news.php">News</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Fokus Pembelajaran
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#Pemograman Web">Pemograman Web</a></li>
+            <li><a class="dropdown-item" href="#Pemograman Desketop">Pemograman Desketop</a></li>
+            <li><a class="dropdown-item" href="#Pemograman Mobile">Pemograman Mobile</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-warning" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<!-- Navbar End -->
+
+<!-- News -->
+<div class="container">
+<div class="row mt-4 p-4">
+      <h1 class="mt-3 text-center fs-1 text-primary font" style="font-weight: bold; font-family: 'Bebas Neue', sans-serif;
+font-family: 'Bricolage Grotesque', sans-serif;">RECENT NEWS</h1>
+<div class="text-center">XI RPL 2 Masih Pemula, Prestasinya Masih Sedikit One Day Bakal Borong Piala</div>
+      <div class="col-md-12 mt-4">
+      <div class="card mb-4">
+    <img src="img/coding 2.jpg" class="card-img-top" alt="coding">
+    <div class="card-body">
+    <h5 class="card-title">Study Tour Ke Bandung</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ex reprehenderit optio deleniti quisquam illo delectus velit obcaecati! Eveniet earum inventore accusamus autem eaque tempore accusantium blanditiis, sapiente fugiat molestiae!</p>
+    <a href="news.php"></a>
+  </div>
+</div>
+<div>
+</div>
+      </div></div></div>
+
+<!-- News End -->
+
+
+
+
+
+    <!-- Footer -->
+    <footer class="bg-primary text-white text-center p-3 mt-5">
+			<p class="mb-0">Desain By <i class="bi bi-emoji-kiss text-warning"></i> <a href="https://www.instagram.com/alienjk7/" class="text-white fw-bold"> Epriya</p>
+		</footer>
+    <!-- Footer End-->
+
+    <script src="bootstrap/js/bootstrap.bundle.js"></script>
+    </body>
+</html>
